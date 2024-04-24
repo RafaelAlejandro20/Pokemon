987b3fc93497368a99923af80c5e269842c477f3
Rafael Alejandro Barajas Urrutia <82453734+RafaelAlejandro20@users.noreply.github.com>
2024-04-23 20:51:19 -0600

    Update PokemonCard.jsx

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./PokemonCard.css"
+import "./PokemonCard.css";
 
 const PokemonCard = ({ pokemon, onSelect }) => {
   const handleCardClick = () => {
@@ -7,8 +7,8 @@ const PokemonCard = ({ pokemon, onSelect }) => {
   };
 
   return (
-    <div className=" card col-sm-5 p-3 col-md-2 m-3 pb-1  rounded border border-secondary d-flex flex-column align-items-center" onClick={handleCardClick}>
-      <p className="col-12 text-end  fs-4 text-success">#{pokemon.id}</p>
+    <div className="card col-sm-5 p-3 col-md-2 m-3 pb-1  rounded border border-secondary d-flex flex-column align-items-center" onClick={handleCardClick}>
+      <p className="col-12 text-end fs-4 text-success">#{pokemon.id}</p>
       <img
         src={pokemon.sprites.other["official-artwork"].front_default}
         alt={pokemon.name}
